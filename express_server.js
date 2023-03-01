9f08c611d69d5a1bb6b3f9f9a8f66b58f51a248f
TheLoneliestTime <john.a.roxas@gmail.com>
2023-02-28 19:49:18 -0800

    added urls_new.ejs and used the boilerplate code from Compass

--- express_server.js
+++ express_server.js
@@ -30,7 +30,6 @@ app.get("/urls", (req, res) => {
 });
 
 app.get("/urls/:id", (req, res) => {
-  console.log(req.params.id);
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id]};
   res.render("urls_show", templateVars);
 });

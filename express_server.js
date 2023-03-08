46a5cd43abbfc687ea04dc10601066d29a8dd378
TheLoneliestTime <john.a.roxas@gmail.com>
2023-03-07 18:09:23 -0800

    implemented mocha/chai as a dev dependency to test helper functions. Moved all helpers functions into helpers.js

--- express_server.js
+++ express_server.js
@@ -2,6 +2,14 @@ const cookieParser = require("cookie-parser");
 const cookiesession = require("cookie-session");
 const bcrypt = require("bcryptjs");
 const express = require("express");
+
+// Helper functions from helper.js
+const {
+  emailLookup,
+  urlsForUser,
+  generateRandomString,
+} = require("./helpers.js");
+
 const app = express();
 const PORT = 8080; // default port 8080
 
@@ -13,41 +21,6 @@ app.use(
     keys: ["key1"],
   })
 );
-
-// Global function to generate a 6 character random string when called.
-const generateRandomString = () => {
-  let result = "";
-  // Declaring all valid characters for the random string. No symbols allowed!
-  const characters =
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-
-  const charLength = characters.length;
-  for (let i = 0; i < 6; i++) {
-    result += characters[Math.floor(Math.random() * charLength)];
-  }
-  return result;
-};
-
-// Global function to lookup whether an email address exists in our database of users. Returns the 6 character userID identifier if found.
-const emailLookup = (email, users) => {
-  for (const key in users) {
-    if (users[key].email === email) {
-      return key;
-    }
-  }
-  return "";
-};
-
-// Global function to sort through all urls for a certain userID.
-const urlsForUser = (id, users) => {
-  const resultObject = {};
-  for (const key in users) {
-    if (users[key].userID === id || users[key].userID === "EXAMPLE") {
-      resultObject[key] = users[key];
-    }
-  }
-  return resultObject;
-};
 
 // Database where all of our short URL IDs and longURL pairs are stored.
 const urlDatabase = {
---
+++ helpers.js
@@ -0,0 +1,40 @@
+// Global function to generate a 6 character random string when called.
+const generateRandomString = () => {
+  let result = "";
+  // Declaring all valid characters for the random string. No symbols allowed!
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  const charLength = characters.length;
+  for (let i = 0; i < 6; i++) {
+    result += characters[Math.floor(Math.random() * charLength)];
+  }
+  return result;
+};
+
+// Global function to lookup whether an email address exists in our database of users. Returns the 6 character userID identifier if found.
+const emailLookup = (email, users) => {
+  for (const key in users) {
+    if (users[key].email === email) {
+      return key;
+    }
+  }
+  return undefined;
+};
+
+// Global function to sort through all urls for a certain userID.
+const urlsForUser = (id, users) => {
+  const resultObject = {};
+  for (const key in users) {
+    if (users[key].userID === id || users[key].userID === "EXAMPLE") {
+      resultObject[key] = users[key];
+    }
+  }
+  return resultObject;
+};
+
+module.exports = {
+  urlsForUser,
+  emailLookup,
+  generateRandomString,
+};
